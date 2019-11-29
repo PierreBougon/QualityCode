27742cc046cad171d60dfbb1ca88a47e3cc99f5a
PierreBougon <pierre.bougon@epitech.eu>
2019-11-29 17:09:52 +0100

    Change test for 100% test coverage

--- src/test/java/utils/LinkedListTest.java
+++ src/test/java/utils/LinkedListTest.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
@@ -35,6 +37,12 @@ class LinkedListTest {
         list.insert(42, 1);
         int res = list.at(1);
         assertEquals(42, res);
+
+        list.insert(1337, 0);
+        res = list.at(0);
+        int res1 = list.at(1);
+        assertEquals(1337, res);
+        assertEquals(1, res1);
     }
 
     @org.junit.jupiter.api.Test
@@ -42,6 +50,10 @@ class LinkedListTest {
         LinkedList<Integer> list = createList();
         list.remove(1);
         int res = list.at(1);
+        assertEquals(3, res);
+
+        list.remove(0);
+        res = list.at(0);
         assertEquals(3, res);
     }
 
@@ -51,6 +63,13 @@ class LinkedListTest {
         list.remove(new Integer(1));
         int res = list.at(0);
         assertEquals(2, res);
+
+        list.remove(new Integer(3));
+        res = list.at(1);
+        assertEquals(4, res);
+
+
+        assertThrows(NoSuchElementException.class, () -> list.remove(new Integer(1222)));
     }
 
     @org.junit.jupiter.api.Test
@@ -58,6 +77,8 @@ class LinkedListTest {
         LinkedList<Integer> list = createList();
         int res = list.at(0);
         assertEquals(1, res);
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> list.at(122));
     }
 
     @org.junit.jupiter.api.Test
@@ -65,6 +86,8 @@ class LinkedListTest {
         LinkedList<Integer> list = createList();
         int res = list.getFirstElementPosition(new Integer(1));
         assertEquals(0, res);
+
+        assertThrows(NoSuchElementException.class, () -> list.getFirstElementPosition(new Integer(1222)));
     }
 
     @org.junit.jupiter.api.Test
@@ -73,6 +96,7 @@ class LinkedListTest {
         list.pushBack(1);
         int res = list.getLastElementPosition(new Integer(1));
         assertEquals(4, res);
+        assertThrows(NoSuchElementException.class, () -> list.getLastElementPosition(new Integer(1222)));
     }
 
     @org.junit.jupiter.api.Test
